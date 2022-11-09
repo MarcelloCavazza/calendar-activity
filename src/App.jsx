a91383532d76b4c8e5471932dd8e749807fdc34a
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-11-09 09:09:10 -0300

    making main creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,121 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const typeOfCreation = [
+    {
+      id: 0,
+      name: 'teachers'
+    },
+    {
+      id: 1,
+      name: 'subjects'
+    },
+    {
+      id: 2,
+      name: 'schoolClass'
+    }
+  ]
+
+  const [teachers, setTeachers] = useState([])
+  const [subjects, setSubjects] = useState([])
+  const [schoolClass, setSchoolClass] = useState([])
+
+  const [createMenu, setCreateMenu] = useState(true)
+  const [searchMenu, setSearchMenu] = useState(false)
+
+  const showCreation = () => {
+    setCreateMenu(true)
+    setSearchMenu(false)
+  }
+
+  const showSearch = () => {
+    setCreateMenu(false)
+    setSearchMenu(true)
+  }
+
+  const createData = (nameOfUser) => {
+    switch (nameOfUser) {
+      case 'teachers':
+        const inputValueTeachers = document.getElementById('teachers')
+        if (inputValueTeachers?.value == undefined || inputValueTeachers?.value == '') {
+          break;
+        }
+
+        let oldTeachers = teachers
+        oldTeachers.push(inputValueTeachers?.value)
+        setTeachers(oldTeachers)
+        break;
+      case 'subjects':
+        const inputValueSubjects = document.getElementById('subjects')
+        if (inputValueSubjects?.value == undefined || inputValueSubjects?.value == '') {
+          break;
+        }
+        let oldsubjects = subjects
+        oldsubjects.push(inputValueSubjects?.value)
+        setSubjects(oldsubjects)
+        break;
+      case 'schoolClass':
+        const inputValueSchoolClass = document.getElementById('schoolClass')
+        if (inputValueSchoolClass?.value == undefined || inputValueSchoolClass?.value == '') {
+          break;
+        }
+        let oldschoolClass = schoolClass
+        oldschoolClass.push(inputValueSchoolClass?.value)
+        setSchoolClass(oldschoolClass)
+        break;
+      default:
+        alert('Erro ao cadastrar')
+        break;
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header>
+        <div>
+          <button onClick={showCreation}>Creation</button>
+          <button onClick={showSearch}>Search</button>
+        </div>
+        <div>Subjects Schedule</div>
+      </header>
+      <div className={createMenu ? 'show' : 'hide'}>
+        <div>
+          {typeOfCreation.map((creation) => {
+          return (
+            <div key={creation.id}>
+              Creation of {creation.name}<br/>
+              <input type="text" name="" id={creation.name} /><br/>
+              <button onClick={_=>createData(creation.name)}>Create</button>
+            </div>
+          )
+          })}
+        </div>
+        
+        <div>
+          <div>Cadastro de Horários</div>
+          <div>
+            <select name="" id="66">
+              {teachers.map(teacher => {
+                return (
+                  <option key={teacher} value={teacher}>{teacher}</option>
+                )
+              })}
+            </select>
+            <select name="" id="77">
+              
+            </select>
+            <select name="" id="88">
+              
+            </select>
+            <input type="text" name="" id="" />
+            <button>Creation</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={searchMenu? 'show':'hide'}>a</div>
     </div>
   )
 }
